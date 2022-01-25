033088b30c512b024b53354de9a7c7485e4f2d8d
montes0022 <73719973+montes0022@users.noreply.github.com>
2022-01-25 15:37:40 -0600

    add updated files and change html files to ejs

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 //Require express and body-parser packages
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 //create app constant using express
 const app = express();
+
+app.set('view engine', 'ejs');
 //Hello when user tries to access / or home route
 app.get("/", function(req, res){
-  res.send("Hello");
+  res.render("home");
 });
 //listen on port 3000, log that the server is running
 app.listen(3000, function(){
